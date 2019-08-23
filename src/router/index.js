e85226843637e2bcab4adb083872cd30f4568a03
long <longhui@yogrt.co>
2019-08-23 20:01:18 +0800

    路由命名错误修改

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const router = new Router({
   routes: [
     {
         path: '/',
-        name: 'home',
+        name: 'faqNew',
         meta:{
             title:'FAQ',
             scrollToTop: true
